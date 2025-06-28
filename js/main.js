470ae5784809b4c795178aa69a6838f2037d20e1
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-06-29 02:14:37 +0500

    update commit

--- js/main.js
+++ js/main.js
@@ -23,12 +23,12 @@ function renderProducts(arr, list){
     list.innerHTML = null
     arr.forEach(item => {
         let elItem = document.createElement("li")
-        elItem.className = "w-[300px] rounded-[30px] overflow-hidden p-4 bg-[#2e2e48] shadow-md"
+        elItem.className = "w-[300px] rounded-[30px] overflow-hidden p-4 bg-white shadow-slate-500 shadow-lg"
         elItem.innerHTML = `
         <img class="h-[300px] w-[230px] mb-3 mx-auto rounded-[30px]" src="${item.image}" alt="img" " />
-        <h2 class="text-[#f5f5f5] font-bold text-[18px] line-clamp-1 mb-2">${item.title}</h2>
-        <p class="text-[#f5f5f5] font-semibold text-[16px] line-clamp-1 mb-1">${item.description}</p>
-        <strong class="text-[#f5f5f5] text-[18px]  mb-2 inline-block">${item.price}$ </strong>
+        <h2 class="text-black font-bold text-[18px] line-clamp-1 mb-2">${item.title}</h2>
+        <p class="text-black font-semibold text-[16px] line-clamp-1 mb-1">${item.description}</p>
+        <strong class="text-black text-[18px]  mb-2 inline-block">${item.price}$ </strong>
         <button onclick="handleOrder(${item.id})" class="w-full cursor-pointer hover:scale-[1.03] hover:bg-transparent hover:text-[#8a2be2] border-[2px] border-[#8a2be2] duration-300 py-2 rounded-md bg-[#8a2be2] text-white font-semibold ">Order</button>
         `
         list.append(elItem)
@@ -39,19 +39,19 @@ function renderProducts(arr, list){
 function handleOrder(id){
     elModalWarapper.classList.remove("scale-0")
     elModalInner.innerHTML = `<img class="w-[30px] h-[30px]" src="./images/loading-white.png" alt="loading img" />`
-    elModalInner.className = "bg-[#1c1c2b] p-5 rounded-[25px]"
+    elModalInner.className = "bg-white shadow-slate-500 shadow-lg p-5 rounded-[25px]"
     axios(`${api}/${id}`).then(res => {
         elModalInner.innerHTML = `
         <div class="flex gap-[30px] ">
         <img class="h-[400px] w-[300px] rounded-[30px]" src="${res.data.image}" alt="img" " width="300" height="300" />
         <div class="w-[300px]">
-        <h2 class=" text-[#f5f5f5] font-bold text-[18px] line-clamp-1 mb-2">${res.data.title}</h2>
-        <p class=" text-[#f5f5f5] font-semibold text-[16px] line-clamp-1 mb-1">${res.data.description}</p>
-        <strong class=" text-[#f5f5f5] text-[18px]  mb-2 inline-block">${res.data.price}$ </strong>
+        <h2 class=" text-black font-bold text-[18px] line-clamp-1 mb-2">${res.data.title}</h2>
+        <p class=" text-black font-semibold text-[16px] line-clamp-1 mb-1">${res.data.description}</p>
+        <strong class=" text-black text-[18px]  mb-2 inline-block">${res.data.price}$ </strong>
         <form class="order-form space-y-3" autocomplete="off">
-        <input type="text" class="text-[#f5f5f5] bg-[#2e2e48] p-3 rounded-md outline-none shadow-md focus:shadow-[#8a2be2] w-full " placeholder="Enter name" name="name"  />
-        <input type="tel" class="text-[#f5f5f5] bg-[#2e2e48] p-3 rounded-md outline-none shadow-md focus:shadow-[#8a2be2] w-full " placeholder="Enter phone number" name="phone"  />
-        <input type="text" class="text-[#f5f5f5] bg-[#2e2e48] p-3 rounded-md outline-none shadow-md focus:shadow-[#8a2be2] w-full " placeholder="Enter address" name="address"  />
+        <input type="text" class="text-black  p-3 rounded-md outline-none shadow-md focus:shadow-[#8a2be2] w-full " placeholder="Enter name" name="name"  />
+        <input type="tel" class="text-black  p-3 rounded-md outline-none shadow-md focus:shadow-[#8a2be2] w-full " placeholder="Enter phone number" name="phone"  />
+        <input type="text" class="text-black  p-3 rounded-md outline-none shadow-md focus:shadow-[#8a2be2] w-full " placeholder="Enter address" name="address"  />
         <button type="submit" class="w-full cursor-pointer hover:scale-[1.03] hover:bg-transparent hover:text-[#8a2be2] border-[2px] border-[#8a2be2] duration-300 py-2 rounded-md bg-[#8a2be2] text-white font-semibold ">Order</button>
         </form>
         </div>
